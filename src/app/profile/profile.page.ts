894361bba7cbf5662c42bd67bffa7af9e49d3939
Asyraf Fuyu <aisraft26@gmail.com>
2019-04-08 23:13:41 +0800

    no null injection anymore

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -15,18 +15,44 @@ export class ProfilePage implements OnInit {
 
   constructor( public afAuth: AngularFireAuth,
   			   public db: AngularFirestore,	) { 
+
   	  this.afAuth.authState.subscribe(user=>{
   		if(user){
   			this.user = user;
   			this.userId = user.uid;
-  			this.db.collection(`users/`+this.userId+`/userProfile`).add({
-		  		age : "",
-		  		weight: "",
-		  		height: "",
-		  		times: 1,
+
+  			// if(!this.db.doc(`users/`+this.userId).ref.get()){
+	  		// 	this.db.collection(`users/`+this.userId+`/userProfile`).add({
+			  // 		age : "",
+			  // 		weight: "",
+			  // 		height: "",
+			  // 		times: 1,
+		  	// })
+  			// }
+  // 		!db.doc(`users/`+this.userId).ref.get().then((documentSnapshot) => {
+  // 			this.db.collection(`users/`+this.userId+`/userProfile`).add({
+		// 	  		age : "",
+		// 	  		weight: "",
+		// 	  		height: "",
+		// 	  		times: 1,
+		//   	})
+		// });
+		this.db.doc(`users/`+this.userId)
+		  .update({})
+		  .then(() => {
+		    // update successful (document exists)
+		  })
+		  .catch((error) => {
+		    // console.log('Error updating user', error); // (document does not exists)
+		    this.db.collection(`users/`+this.userId+`/userProfile`).add({
+			  		age : "",
+			  		weight: "",
+			  		height: "",
+			  		times: 1,
 		  	})
-  			console.log(this.userId);
-  			}
+ 		 });
+  		}
+  		
   		})
   }
 
