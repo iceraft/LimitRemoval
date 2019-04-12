5328d141ec5b6f7899ca6c67070cbd929c4a867e
Asyraf Fuyu <aisraft26@gmail.com>
2019-04-12 10:51:30 +0800

    unkownly resolved the type never error|`052/12/04/2019

--- src/app/workout/workout.page.ts
+++ src/app/workout/workout.page.ts
@@ -1,11 +1,75 @@
+// import { Component, OnInit } from '@angular/core';
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFirestore } from '@angular/fire/firestore';
+// import { ModalController } from '@ionic/angular';
+// import { AngularFireAuth } from '@angular/fire/auth';
+// import { WorkoutAddPage } from './workout-add/workout-add.page';
+// // import { WorkoutPlayPage } from './workout-play/workout-play.page';
+
+
+// @Component({
+//   selector: 'app-workout',
+//   templateUrl: './workout.page.html',
+//   styleUrls: ['./workout.page.scss'],
+// })
+// export class WorkoutPage implements OnInit {
+
+//   myRegiments: [];
+//   regiments: [];
+//   alarms: [];
+
+//   constructor(public afAuth: AngularFireAuth,
+//   			      public mc: ModalController,	
+//               public db: AngularFirestore,) { }
+
+//   ngOnInit() {
+//     this.db.collection('alarms').stateChanges().subscribe(serverAlarms =>{
+//       this.alarms=[];
+//       serverAlarms.forEach(a=>{
+//         let alarm:any = a.payload.doc.data();
+//         alarm.id = a.payload.doc.id;
+//         this.alarms.push(alarm);
+//       });
+//     });
+
+
+//     this.db.collection('regiments').snapshotChanges().subscribe(serverRegi =>{
+//       this.regiments=[];
+//       serverRegi.forEach(a=>{
+//          let regiment:any = a.payload.doc.data();
+//          regiment.id = a.payload.doc.id;
+//         //type never wtf is that shit bruh?
+//         //this.regiments.push(a.payload.doc.data());
+//         console.log(a);
+//         console.log(this.regiments);
+//       });
+//     });
+//   }
+
+//   async add() {
+//     const modal = await this.mc.create({
+//       component: WorkoutAddPage,
+//       backdropDismiss: false,
+//     });
+//     return await modal.present();
+//   }
+
+
+//   // async play() {
+//   //   const modal = await this.mc.create({
+//   //     component: WorkoutAddPage,
+//   //     backdropDismiss: false,
+//   //   });
+//   //   return await modal.present();
+//   // }
+// } 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import { Component, OnInit } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ModalController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { WorkoutAddPage } from './workout-add/workout-add.page';
-// import { WorkoutPlayPage } from './workout-play/workout-play.page';
-
 
 @Component({
   selector: 'app-workout',
@@ -13,25 +77,30 @@ import { WorkoutAddPage } from './workout-add/workout-add.page';
   styleUrls: ['./workout.page.scss'],
 })
 export class WorkoutPage implements OnInit {
+  
+  regiments = [];
 
-  myRegiments: [];
-  regiments: [];
+  constructor(private db: AngularFirestore,
+              private mc: ModalController,
+              public afAuth: AngularFireAuth,) { }
 
-  constructor(public afAuth: AngularFireAuth,
-  			      public mc: ModalController,	
-              public db: AngularFirestore,) { }
 
-  ngOnInit() {
-    this.db.collection('regiments').snapshotChanges().subscribe(serverRegi =>{
+ ngOnInit() {
+this.db.collection('regiments').snapshotChanges().subscribe(serverRegi =>{
       this.regiments=[];
       serverRegi.forEach(a=>{
-        // let regiment:any = a.payload.doc.data();
-        // regiment.id = a.payload.doc.id;
+         let regiment:any = a.payload.doc.data();
+         regiment.id = a.payload.doc.id;
         //type never wtf is that shit bruh?
-        //this.regiments.push(a.payload.doc.data());
+        this.regiments.push(regiment);
+        console.log(a);
+        console.log(this.regiments);
       });
     });
-  }
+
+  };
+
+  
 
   async add() {
     const modal = await this.mc.create({
@@ -41,12 +110,18 @@ export class WorkoutPage implements OnInit {
     return await modal.present();
   }
 
-
-  // async play() {
-  //   const modal = await this.mc.create({
-  //     component: WorkoutAddPage,
+  // delete(alarm){
+  //     this.db.doc('alarms/'+alarm.id).delete();
+  // }
+ 
+  //  async edit(alarm) {
+  //   const modal = await this.modalCtrl.create({
+  //     component: AlarmEditPage,
   //     backdropDismiss: false,
+  //     componentProps: {
+  //     alarm: alarm,
+  //  }
   //   });
   //   return await modal.present();
-  // }
-} 
+  //  }
+}
